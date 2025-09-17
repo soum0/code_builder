f4f11e89b57a2da730176019c035c60b2ef1118b
Soum Singh <soumsingh@Soums-MacBook-Air.local>
2025-09-17 21:21:09 +0530

    Initial commit

---
+++ Agent/graph.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+from pydantic import BaseModel
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Schema(BaseModel):
+    price : float
+    eps : float
+
+resp = llm.with_structured_output(Schema).invoke('Extract price and eps from this text: "The price is 100.5 and the eps is 5.6"')
+print(resp)
